72624b90ca2d26793d504a6092319cce7f3c72e0
merndeveloperdhke <merndevelpler@gmail.comm>
2023-05-20 19:57:15 +0600

    summery

---
+++ summery.js
@@ -0,0 +1,13 @@
+// 1. Optional chaining
+//  student.marks?.math
+
+// must
+// 2. map, filter, find
+
+// 3. (Optional) : forEach, reduce (advanced)
+
+// 4. class
+
+// 5. (Optional): inheritance, super
+
+// 6. (must) prototypical inheritance
